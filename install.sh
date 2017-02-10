58bfef8a04d6ace697d7f278d265bc4dbe1dd09a
cassia233 <cassia02200059@gmail.com>
2017-02-10 15:03:11 +0800

    fix shell errors

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-i
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #link dotfiles
-ln -s ./.vimrc  ~/.vimrc
-ln -s ./.tmux.conf ~/.tmux.conf
-ln -s ./.Xdefaults ~/.Xdefaults
-ln -s ./.npmrc ~/.npmrc
-ln -s ./.zshrc  ~/.zshrc
-mkdir /tmp/vimundo
+ln -sf $HOME/dotfiles/.vimrc  $HOME/.vimrc
+ln -sf $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -sf $HOME/dotfiles/.Xdefaults $HOME/.Xdefaults
+ln -sf $HOME/dotfiles/.npmrc $HOME/.npmrc
+ln -sf $HOME/dotfiles/.zshrc  $HOME/.zshrc
+mkdir -p /tmp/vimundo
 
 vim +PlugInstall
